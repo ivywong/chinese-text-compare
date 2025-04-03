e3f02f35d92804ffb345cb9f48923c3fed3d6dba
Ivy Wong <ivy@ivywong.dev>
2025-04-03 16:03:01 -0400

    extract basic word/char data from text

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,46 @@ import './App.css';
 
 import { cut } from 'jieba-wasm';
 
+type TextData = {
+  title: string;
+  totalCharacters: number;
+  totalWords: number;
+  uniqueCharacters: number;
+  uniqueWords: number;
+};
+
 function App() {
   const fileInputRef = React.useRef<HTMLInputElement>(null);
 
-  const [parsedContent, setParsedContent] = React.useState<string>('');
+  const [data, setData] = React.useState<Array<TextData>>([]);
+
+  function addTextData(title: string, text: string) {
+    console.log(text);
+
+    const regexp = /[\p{sc=Han}]/gu; // match only hanzi, theoretically
+    const filteredChars = text.match(regexp) ?? [];
+
+    // HMM disabled to match CTA stats, seems to result in more word combos
+    const totalWords = cut(text, false);
+    const filteredWords = totalWords
+      .filter((word) => {
+        return word.match(regexp);
+      })
+      .sort();
+
+    const uniqueChars = new Set(filteredChars);
+    const uniqueWords = new Set(filteredWords);
+
+    const newData: TextData = {
+      title,
+      totalCharacters: filteredChars.length,
+      totalWords: filteredWords.length,
+      uniqueCharacters: uniqueChars.size,
+      uniqueWords: uniqueWords.size,
+    };
+
+    setData([...data, newData]);
+  }
 
   async function handleFileUpload(e: React.FormEvent) {
     e.preventDefault();
@@ -17,14 +53,11 @@ function App() {
 
     const text = await file?.text();
 
-    if (!text) {
+    if (!file?.name || !text) {
       throw Error('failed to read file: ' + file);
     }
 
-    console.log(text);
-    console.log(cut(text));
-
-    setParsedContent(cut(text).toString());
+    addTextData(file.name, text);
   }
 
   return (
@@ -32,7 +65,7 @@ function App() {
       <h1>Chinese Text Compare</h1>
       <form onSubmit={handleFileUpload}>
         <input ref={fileInputRef} type="file" id="fileUpload"></input>
-        <pre id="parsed-content">{parsedContent}</pre>
+        <pre id="parsed-content">{data.map((obj) => JSON.stringify(obj)).join('\n')}</pre>
         <button type="submit">Upload Text</button>
       </form>
     </>
